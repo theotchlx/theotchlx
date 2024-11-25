744c2da92093788e8be59607833cf73033f14488
Théo Tchilinguirian <theo.tchlx@gmail.com>
2024-11-25 15:05:52 +0100

    feat: update content and style

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,10 +2,10 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
   return rss({
-    title: 'theotchlx | Blog',
-    description: 'Blog articles made by me!',
+    title: 'theotchlx | Articles',
+    description: 'Quality articles made by me, for you :)',
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    items: await pagesGlobToRssItems(import.meta.glob('./pages/blog/**/*.md')),
     customData: `<language>en-us</language>`,
   });
 }
