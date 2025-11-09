ca82acde92895529e968952c2530d024c13b8a90
Théo Tchilinguirian <theo.tchlx@proton.me>
2025-11-09 16:10:02 +0100

    fix(rss): add email to RSS author field

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -7,7 +7,7 @@ export async function GET(context) {
     site: context.site,
     items: await pagesGlobToRssItems(import.meta.glob('./blog/**/*.md')),
     customData: `<language>en-us</language>`,
-    author: 'Théo Tchilinguirian',
+    author: 'theo.tchlx@proton.me (Théo Tchilinguirian)',
     link: `${context.site}/rss.xml`,
   });
 }
